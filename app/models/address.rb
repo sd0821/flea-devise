36c2e0bab946dad0daeabaded643b6264fa1a9cc
sd0821 <68905745+sd0821@users.noreply.github.com>
2020-11-01 21:08:55 +0900

    ウィザード実装２

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,13 +1,27 @@
 class Address < ApplicationRecord
-  validates :destination_family_name, :destination_first_name, :destination_family_name_furigana, :destination_family_name_furigana, :postal_code, :municipality, :house_number, presence: true
-  # 空の入力を防ぐ
-  validates :tel, uniqueness: true, allow_blank: true
-  # 同じ電話番号は登録できない
-  validates :destination_family_name, :destination_first_name, format: {with:/\A[ぁ-んァ-ン一-龥]/, message: "は全角のみで入力して下さい"}
-  # 名前 全角文字のみ登録可能
-  validates :destination_family_name_furigana, :destination_family_name_furigana, format: {with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナのみで入力して下さい"}
-  # 名前よみがな 全角カタカナにみ登録可能
+
+  validates :destination_family_name,
+  format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角ひらがな、全角カタカナ、漢字のみが使用できます"},
+  presence: true
+  validates :destination_first_name,
+  format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角ひらがな、全角カタカナ、漢字のみが使用できます"},
+  presence: true
+  validates :destination_family_name_furigana,
+  format: { with: /\A([ァ-ン]|ー)+\z/, message: "全角カタカナのみが使用できます"},
+  presence: true
+  validates :destination_first_name_furigana,
+  format: { with: /\A([ァ-ン]|ー)+\z/, message: "全角カタカナのみが使用できます"},
+  presence: true
+  validates :postal_code,
+  format: { with: /\A\d{3}[-]\d{4}\z/,message: "ハイフン入り７桁で入力してください"},
+  presence: true
+  validates :prefectures_area, presence: true
+  validates :municipality, presence: true
+  validates :house_number, presence: true
+  validates :tel,
+  format: { with: /\A\d{10}\z/, message: "半角数字のみが使用できます"}
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+  belongs_to :user, optional: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,27 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, uniqueness: true 
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :familyname_furigana, presence: true
-  validates :firstname_furigana, presence: true
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角ひらがな、全角カタカナ、漢字のみが使用できます"}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角ひらがな、全角カタカナ、漢字のみが使用できます"}
+  validates :familyname_furigana, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/, message: "全角カタカナのみが使用できます"}
+  validates :firstname_furigana, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/, message: "全角カタカナのみが使用できます"}
   validates :date, presence: true
+
+  # validates :nickname, presence: true
+  # validates :family_name,
+  #   format: { with: /\A[ぁ-んァ-ン一-龥]/},
+  #   presence: true
+  # validates :filst_name,
+  #   format: { with: /\A[ぁ-んァ-ン一-龥]/},
+  #   presence: true
+  # validates :familyname_furigana,
+  #   format: { with: /\A([ァ-ン]|ー)+\z/},
+  #   presence: true
+  # validates :filst_name_furigana,
+  #   format: { with: /\A([ァ-ン]|ー)+\z/},
+  #   presence: true
+  # validates :date,
+  #   presence: true
 
   has_one :address
 end
