7901490dd7c7031dc9982b5019056ba64b32b0bf
sd0821 <68905745+sd0821@users.noreply.github.com>
2020-11-05 10:13:44 +0900

    ウィザート訂正分

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, password_length: 7..128
 
-  validates :nickname, presence: true, uniqueness: true 
+  validates :nickname, presence: true
   validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角ひらがな、全角カタカナ、漢字のみが使用できます"}
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角ひらがな、全角カタカナ、漢字のみが使用できます"}
   validates :familyname_furigana, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/, message: "全角カタカナのみが使用できます"}
